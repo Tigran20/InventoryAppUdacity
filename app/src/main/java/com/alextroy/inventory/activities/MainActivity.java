66427bf68a1e02e11678e9287943246af9049cad
Tigran20 <Podliza122@bk.ru>
2018-05-10 20:27:16 +0300

    added the emptyView

--- app/src/main/java/com/alextroy/inventory/activities/MainActivity.java
+++ app/src/main/java/com/alextroy/inventory/activities/MainActivity.java
@@ -6,11 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alextroy.inventory.R;
 import com.alextroy.inventory.adapter.InventoryAdapter;
 import com.alextroy.inventory.model.Data;
-import com.alextroy.inventory.model.InventoryDbHelper;
 import com.alextroy.inventory.model.Product;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private InventoryAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private FloatingActionButton actionButton;
+    private TextView emptyView;
 
     private static List<Product> list;
 
@@ -35,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         list = Data.getProductData(this);
 
+        emptyView = findViewById(R.id.empty_view);
         recyclerView = findViewById(R.id.recycler_view);
         actionButton = findViewById(R.id.fab);
 
@@ -42,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         adapter = new InventoryAdapter(getApplicationContext(), list);
         recyclerView.setAdapter(adapter);
+
+        if (list.isEmpty()) {
+            recyclerView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        }
 
         actionButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
